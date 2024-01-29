ce74ca6fbebda54d979bd0ddeb61939452710f0f
Mathias Smeets <mathias_smeets@hotmail.com>
2024-01-29 11:14:29 +0100

    ica start

---
+++ Mathias/clusters_ica.m
@@ -0,0 +1,23 @@
+clear; clc; close all;
+
+%% get data
+
+path_to_code = "\\nerffs13\takeokalabwip2020\Mathias\data\";
+%path_to_code = "/mnt/takeokalab/takeokalabwip2023/Mathias/10kfs/";
+%other_path = "/mnt/takeokalab/takeokalabwip2023/Mathias/data/";
+
+stimulus_data_m = load(path_to_code + "data_after_stimulus_m.mat");
+stimulus_data_m = stimulus_data_m.after_stimulus_data_m;
+stimulus_data_y = load(path_to_code + "data_after_stimulus_y.mat");
+stimulus_data_y = stimulus_data_y.after_stimulus_data_y;
+
+%% put this in matrix format
+mouse_1_m = stimulus_data_m{1,1};
+for i = 1:size(stimulus_data_m,2)
+    if ~isempty(stimulus_data_m{1,i})
+        mouse_1_m = [mouse_1_m, stimulus_data_m{1,i}];
+    end
+end
+
+%% arnoud function
+[predicted_nbr_assemblies, predicted_nbr_neurons,assemblies,activity] = ica_assembly_detection(mouse_1_m,1);

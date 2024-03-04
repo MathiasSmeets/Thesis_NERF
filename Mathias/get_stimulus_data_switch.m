5d953f7fca76812e320d725e546c294688fccc7a
Mathias Smeets <mathias_smeets@hotmail.com>
2024-03-04 11:14:10 +0100

    update

--- Mathias/get_stimulus_data_switch.m
+++ Mathias/get_stimulus_data_switch.m
@@ -214,15 +214,15 @@ for i = 1:numel(fields)
             %after_stimulus_data_m{i,j-1}(start_neuron_m:start_neuron_m + cur_nb_neurons_m - 1, round(cur_onsets_m(j)*1000)+1:round(cur_onsets_m(j)*1000)+1) = 0;
         end
     end
-    for j = 2:length(cur_onsets_y)
-        if round(cur_onsets_y(j)*1000)+59 > 600000
-            %after_stimulus_data_y{i,j-1} = switch_y(start_neuron_y:start_neuron_y + cur_nb_neurons_y - 1,round(cur_onsets_y(j)*1000)-10+2:end);
-            %after_stimulus_data_y{i,j-1}(start_neuron_y:start_neuron_y + cur_nb_neurons_y - 1, round(cur_onsets_y(j)*1000)+1:round(cur_onsets_y(j)*1000)+1) = 0;
-        else
-            %after_stimulus_data_y{i,j-1} = switch_y(start_neuron_y:start_neuron_y + cur_nb_neurons_y - 1,round(cur_onsets_y(j)*1000)-10+2:round(cur_onsets_y(j)*1000)+59+2);
-            %after_stimulus_data_y{i,j-1}(start_neuron_y:start_neuron_y + cur_nb_neurons_y - 1, round(cur_onsets_y(j)*1000)+1:round(cur_onsets_y(j)*1000)+1) = 0;
-        end
-    end
+    % for j = 2:length(cur_onsets_y)
+    %     if round(cur_onsets_y(j)*1000)+59 > 600000
+    %         after_stimulus_data_y{i,j-1} = switch_y(start_neuron_y:start_neuron_y + cur_nb_neurons_y - 1,round(cur_onsets_y(j)*1000)-10+2:end);
+    %         after_stimulus_data_y{i,j-1}(start_neuron_y:start_neuron_y + cur_nb_neurons_y - 1, round(cur_onsets_y(j)*1000)+1:round(cur_onsets_y(j)*1000)+1) = 0;
+    %     else
+    %         after_stimulus_data_y{i,j-1} = switch_y(start_neuron_y:start_neuron_y + cur_nb_neurons_y - 1,round(cur_onsets_y(j)*1000)-10+2:round(cur_onsets_y(j)*1000)+59+2);
+    %         after_stimulus_data_y{i,j-1}(start_neuron_y:start_neuron_y + cur_nb_neurons_y - 1, round(cur_onsets_y(j)*1000)+1:round(cur_onsets_y(j)*1000)+1) = 0;
+    %     end
+    % end
     start_neuron_m = start_neuron_m + cur_nb_neurons_m;
     %start_neuron_y = start_neuron_y + cur_nb_neurons_y;
 end

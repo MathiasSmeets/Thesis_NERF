1d9517a585c6e675b3922d913a26717d82c5ae75
Mathias Smeets <mathias_smeets@hotmail.com>
2024-03-18 16:10:45 +0100

    update

--- Mathias/replay_switch.m
+++ Mathias/replay_switch.m
@@ -37,7 +37,7 @@ last_interval_data = zeros(1,size(stimulus_data_m,1));
 % cur_activity_before = zeros(size(stimulus_data_m,1),size(before_data_m,2)-1-size(cur_template,2)+1);
 % cur_activity_after = zeros(size(stimulus_data_m,1),size(after_data_m,2)-1-size(cur_template,2)+1);
 % cur_activity_between = cell(size(stimulus_data_m,1),1);
-adj_cur_correlation_horridge = zeros(size(stimulus_data_m,1),1);
+adj_cur_correlation_horridge = cell(size(stimulus_data_m,1),1);
 adj_cur_correlation_switch = cell(size(stimulus_data_m,1),1);
 for i = 1:size(stimulus_data_m,1)
     % get last interval

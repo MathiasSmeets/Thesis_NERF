7aa5b7d692bbd8cd20780d79e432d1fd4b1534d4
Mathias Smeets <mathias_smeets@hotmail.com>
2024-03-21 20:20:05 +0100

    update

--- Mathias/replay.m
+++ Mathias/replay.m
@@ -125,29 +125,30 @@ for i = setdiff(1:size(stimulus_data_m,1),mouse_to_exclude)
     cur_value = maxvalue(value_counter);
     most_common_cluster = all_assemblies(all_assemblies_count==cur_value);
     actual_most_common_cluster = most_common_cluster{end};
-    previous_mouse = 0;
+    internal_counter = 0;
+    % contains neuron to avoid
     while ~isempty(find(ismember(actual_most_common_cluster, neurons_to_avoid), 1))
+        % if another has equal count
         if ~isequal(actual_most_common_cluster, most_common_cluster{1})
-            if previous_mouse ~= i
-                internal_counter = 1;
-            else
-                internal_counter = internal_counter + 1;
-            end
+            internal_counter = internal_counter + 1;
             actual_most_common_cluster = most_common_cluster{end-internal_counter};
+        % else go to next count
         else
             value_counter = value_counter + 1;
             cur_value = maxvalue(value_counter);
             most_common_cluster = all_assemblies(all_assemblies_count==cur_value);
             actual_most_common_cluster = most_common_cluster{end};
         end
+        % if count is one and we went over all, just take the most common anyways
         if cur_value == 1 && isequal(actual_most_common_cluster, most_common_cluster{1})
             cur_value = maxvalue(1);
             most_common_cluster = all_assemblies(all_assemblies_count==cur_value);
             actual_most_common_cluster = most_common_cluster{end};
             break;
         end
+        disp('x')
     end
-
+    disp(i)
 
     % while ~isempty(find(cellfun(@(x) isequal(x, most_common_cluster), active_assemblies_before), 1))
     %     value_counter = value_counter+1;

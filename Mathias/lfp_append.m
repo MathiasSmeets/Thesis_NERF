a42d125152fc225dac5aa1755458644ea31cc009
Mathias Smeets <mathias_smeets@hotmail.com>
2024-05-11 20:10:12 +0200

    update

---
+++ Mathias/lfp_append.m
@@ -0,0 +1,12 @@
+clear;clc;close all;
+
+for i = setdiff(1:9,[2,8,9])
+    disp(i)
+    % do it in multiple parts
+    data = [];
+    for j = 1:5
+        load("/mnt/takeokalab/takeokalabwip2023/Mathias/switch_data/LF_signals/LF_filtered_rec"+i+"_part"+j+"m.mat")
+        data = [data;cur_filtered_data];
+    end
+    save("/scratch/mathiass-takeokalab/01/LF_filtered_total"+i+"m.mat", "data", "-v7.3");
+end

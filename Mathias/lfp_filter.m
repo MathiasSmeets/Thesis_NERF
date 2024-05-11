69b44835cbeddcc363f35cfec76d7164fd7c0c8f
Mathias Smeets <mathias_smeets@hotmail.com>
2024-05-11 10:49:07 +0200

    update

--- Mathias/lfp_filter.m
+++ Mathias/lfp_filter.m
@@ -10,7 +10,7 @@ for i = setdiff(1:9,[2,8,9])
     for j = 1:5
         disp(i+": "+j)
         load("/mnt/takeokalab/takeokalabwip2023/Mathias/switch_data/LF_signals/LF_"+i+"m.mat")
-        size_data = size(data,2);
+        size_data = size(data,2)/5;
         cur_data = data(:,1+(j-1)*size_data:j*size_data);
         clearvars data
         cur_filtered_data = filtfilt(filter,cur_data');

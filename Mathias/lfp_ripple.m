990a24c576f38df7cfc6d4dd364fbc2dab24b7b2
Mathias Smeets <mathias_smeets@hotmail.com>
2024-05-12 10:31:40 +0200

    update

--- Mathias/lfp_ripple.m
+++ Mathias/lfp_ripple.m
@@ -7,9 +7,10 @@ for i = setdiff(1:9,[2,8,9])
     load("/mnt/takeokalab/takeokalabwip2023/Mathias/switch_data/LF_signals/LF_filtered_total"+i+"m.mat")
     data = downsample(data,25);
     ripple = zeros(size(data,1),1);
+    excluded = [];
     for j = 5*Fs/25+1:size(data,1)-1
         disp(j+"/"+size(data,1))
-        cur_data = data(j-window:j,:);
+        cur_data = data(setdiff(j-window:j,excluded),:);
         cur_power = cur_data.^2;
         cur_power = sum(cur_power,2);
         cur_average = mean(cur_power);
@@ -17,6 +18,7 @@ for i = setdiff(1:9,[2,8,9])
         
         if 5*cur_mad + cur_average < (sum(data(j+1,:).^2))
             ripple(j+1) = 1;
+            excluded = [excluded, j+1];
         end
     end
     save("/scratch/mathiass-takeokalab/01/ripple_"+i+".mat", "ripple", "-v7.3");

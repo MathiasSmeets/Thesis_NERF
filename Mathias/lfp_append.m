2a0d20532de28ac47a5f9500ab8593a443461277
Mathias Smeets <mathias_smeets@hotmail.com>
2024-05-13 21:50:20 +0200

    update

--- Mathias/lfp_append.m
+++ Mathias/lfp_append.m
@@ -5,8 +5,8 @@ for i = setdiff(1:9,[2,8,9])
     % do it in multiple parts
     data = [];
     for j = 1:5
-        load("/mnt/takeokalab/takeokalabwip2023/Mathias/switch_data/LF_signals/LF_filtered_rec"+i+"_part"+j+"m.mat")
+        load("/mnt/takeokalab/takeokalabwip2023/Mathias/switch_data/LF_signals/LF_filtered_rec"+i+"_part"+j+"m_ripple2.mat")
         data = [data;cur_filtered_data];
     end
-    save("/scratch/mathiass-takeokalab/01/LF_filtered_total"+i+"m.mat", "filtered_data", "-v7.3");
+    save("/scratch/mathiass-takeokalab/01/LF_filtered_total"+i+"m_ripple2.mat", "filtered_data", "-v7.3");
 end

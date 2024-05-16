7e3cedae43f804271785f9752b322e45a251c847
Mathias Smeets <mathias_smeets@hotmail.com>
2024-05-16 20:40:01 +0200

    update

--- Mathias/get_lfp_data.m
+++ Mathias/get_lfp_data.m
@@ -8,17 +8,30 @@ freqBands = {[0.5 4], [4 8], [8 12], [12 35], [35 200]};
 nChansInFile = 385;  % neuropixels phase3a, from spikeGLX
 
 % - - - - - - - To study a unique recording
-possiblePaths={'/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220725/20220725_long_long_recording_g0';
-        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220819/20220819_long_long_recording2_g0';%Recording seems to be corrupted or so, only 606 seconds long
-        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220831/20220831_long_long_recording_g0';
-        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220921/20220921_long_long_recording_g0';
-        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220923/20220923_long_long_recording_g0';
-        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20221011/20221011_long_long_recording_g0';
-        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20221116/20221116_long_long_recording_g0';
-        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20221124/20221124_long_long_recording2_g0';
-        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20221128/20221128_long_long_recording_g1';
-        };
+
+% LEARNER
+% possiblePaths={'/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220725/20220725_long_long_recording_g0';
+%         '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220819/20220819_long_long_recording2_g0';%Recording seems to be corrupted or so, only 606 seconds long
+%         '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220831/20220831_long_long_recording_g0';
+%         '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220921/20220921_long_long_recording_g0';
+%         '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220923/20220923_long_long_recording_g0';
+%         '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20221011/20221011_long_long_recording_g0';
+%         '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20221116/20221116_long_long_recording_g0';
+%         '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20221124/20221124_long_long_recording2_g0';
+%         '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20221128/20221128_long_long_recording_g1';
+%         };
  
+% CONTROL
+possiblePaths={'/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220705/20220705_long_long_recording_g0';
+        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220725/20220725_long_long_recording_g0';%Recording seems to be corrupted or so, only 606 seconds long
+        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220905/20220905_long_long_recording_g0';
+        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220922/20220922_long_long_recording_g0';
+        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220927/20220927_long_long_recording_g0';
+        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20221006/20221006_long_long_recording_g0';
+        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20221018/20221018_long_long_recording_g0';
+        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220906/20220906_long_long_recording2_g0';
+        '/mnt/takeokalab/takeokalabwip2020/past lab members/Mattia/Spinal_Cord/Recordings/20220413/20220413_long_long_recording_g1';
+        };
 
 rest_start_times = [3665,3659,3655,3709,3921,3940,3040,3075,3207];
 rest_end_times = [8700,7270,7270,7382,7924,7563,6902,7212,7213];
@@ -47,7 +60,7 @@ for i = [2,8,9]
     mmf = memmapfile(lfpFilename, 'Format', {'int16', [nChansInFile nSamps], 'x'});
 
     data = double(mmf.Data(1).x(:, (sampStarts:sampEnds)));
-    save("/scratch/mathiass-takeokalab/01/LF_"+i+"m.mat", "data", "-v7.3");
+    save("/scratch/mathiass-takeokalab/01/LF_"+i+"y.mat", "data", "-v7.3");
     clearvars data
     % filtered_data = zeros(length(freqBands),size(data,2),size(data,1));
     % for j = 1:length(freqBands)

e5edc7da284ba2254d13e479f26bef257ccfe2c6
Mathias Smeets <mathias_smeets@hotmail.com>
2024-05-16 22:20:19 +0200

    update

--- Mathias/lfp_filter.m
+++ Mathias/lfp_filter.m
@@ -3,7 +3,7 @@ Fs = 2500;
 frequencies = [135,255];
 
 
-for i = 9%1:9
+for i = 1:9
     disp(i)
     % do it in multiple parts
     %filter = designfilt('bandpassiir','FilterOrder',20,'PassbandFrequency1',11,'PassbandFrequency2',16,'StopbandAttenuation1',60,'PassbandRipple',1,'StopbandAttenuation2',60,'SampleRate',2500,'DesignMethod','ellip');
@@ -11,7 +11,7 @@ for i = 9%1:9
     %filter_spindle = designfilt('bandpassiir','FilterOrder',20,'HalfPowerFrequency1',11,'HalfPowerFrequency2',20,'SampleRate',2500,'DesignMethod','butter');
     for j = 5%1:5
         disp(i+": "+j)
-        load("/mnt/takeokalab/takeokalabwip2023/Mathias/switch_data/LF_signals/LF_"+i+"m.mat")
+        load("/mnt/takeokalab/takeokalabwip2023/Mathias/switch_data/LF_signals/LF_"+i+"y.mat")
         size_data = floor(size(data,2)/5);
         cur_data = data(:,1+(j-1)*size_data:j*size_data);
         clearvars data

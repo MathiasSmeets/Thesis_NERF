3452cea91d7bb9629a085204580713b363013f4d
Mathias Smeets <mathias_smeets@hotmail.com>
2024-05-21 14:44:35 +0200

    update

--- Mathias/lfp_get_power.m
+++ Mathias/lfp_get_power.m
@@ -1,7 +1,7 @@
 clear;clc;close all;
 Fs = 2500;
 
-for i = 1:9
+for i = 2:9
     disp(i)
     %load("/mnt/takeokalab/takeokalabwip2023/Mathias/switch_data/LF_signals/LF_filtered_total"+i+"m.mat")
     data = [];
@@ -11,8 +11,8 @@ for i = 1:9
         data = [data;cur_filtered_data];
     end
     % calculate power
-    cur_power = data.^2;
-    cur_power = sum(cur_power,2);
+    power = data.^2;
+    power = sum(power,2);
 
     save("/scratch/mathiass-takeokalab/01/power_"+i+"m.mat", "power", "-v7.3");
 end

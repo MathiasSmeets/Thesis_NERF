8c6cb078c029c035399413d0f5ffde9b0f96ac6c
Mathias Smeets <mathias_smeets@hotmail.com>
2024-05-23 14:24:32 +0200

    update

---
+++ Mathias/lfp_low_frequency_power_analysis.m
@@ -0,0 +1,10 @@
+clear;clc;close all;
+
+for i = 1:9
+    cur_fig = openfig("X:\Mathias\01\01\powerfigure_theta_"+i+".fig");
+    cur_ax_objects = cur_fig.Children;
+    cur_data = cur_ax_objects.Colordata;
+
+    
+
+end
